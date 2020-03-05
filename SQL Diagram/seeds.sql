f07e990638d1a48d15e8e38e035c0fa0bd7593df
dalex <delphindidier0@gmail.com>
2020-03-05 02:59:39 -0500

    Updated Changes 6

---
+++ SQL Diagram/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO panda (panda_name) VALUES ('');
+INSERT INTO panda (panda_name) VALUES ('');
+INSERT INTO panda (panda_name) VALUES ('');
+
+INSERT INTO panda (part_name, p_type, pa_cost, c_id) VALUES ('Everybody s ', 't', 500, 1);
+INSERT INTO panda (part_name, p_type, pa_cost, c_id) VALUES ('Big g ', 't', 900, 1);
+INSERT INTO panda (part_name, p_type, pa_cost, c_id) VALUES ('Top ', 'm', 200, 2);
+INSERT INTO panda (part_name, p_type, pa_cost, c_id) VALUES ('key', 'gr', 300, 2);
+INSERT INTO panda (part_name, p_type, pa_cost, c_id) VALUES ('gar', 'gr', 250, 3);
+ 
+ 
