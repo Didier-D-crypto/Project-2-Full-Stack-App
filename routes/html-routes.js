a38ed437443ed777e191e85696356813f8ee47b4
Your Name <you@example.com>
2020-03-09 14:03:05 -0400

    login is start page, and user can go back-and-forth between signup and login pages.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,27 +6,20 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   
-  /* Sign-Up Is Now Log-In Path: It says "Or log in here" at bottome of page */
-  
   app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-
-/* login.html was deleted.  It's display is on "signup.html", though. */
-
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
-  /* This path was changed to main.html */ 
+  app.get("/signup", function(req, res) {
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
   app.get("/members", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
@@ -34,7 +27,6 @@ module.exports = function(app) {
   app.get("/itinerary", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/itinerary.html"));
   });
-};
-
-
+}
+  
 
