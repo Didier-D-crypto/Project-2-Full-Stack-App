cc39b28f5919135f9f890516fb250c230a610aa6
Alyssa Cucunato <acucunato18@gmail.com>
2020-03-09 21:23:59 -0400

    updated event api call, updated format for exisiting itineraries, added favicon

--- models/Cities.js
+++ models/Cities.js
@@ -1,41 +1,33 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  
-  const Cities = sequelize.define('Cities', {
+  const Cities = sequelize.define("Cities", {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
-      primaryKey: true,
-
+      primaryKey: true
     },
     name: DataTypes.STRING,
     itineraries_id: {
       type: DataTypes.INTEGER,
       required: true,
-<<<<<<< HEAD
-      allowNull: true,
+      allowNull: true
     },
     createdAt: {
-      field: 'created_at',
-      type: DataTypes.DATE,
-
-  },
-  updatedAt: {
-    
-      field: 'updated_at',
-      type: DataTypes.DATE,
-
-  }
-
-    
-
-  });
-  Cities.associate = (models) => {
-    Cities.hasMany(models.Itineraries), {
-      foreignKey: { 
-        allowNull: true
-      }
+      field: "created_at",
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      field: "updated_at",
+      type: DataTypes.DATE
     }
+  });
+  Cities.associate = models => {
+    Cities.hasMany(models.Itineraries),
+      {
+        foreignKey: {
+          allowNull: true
+        }
+      };
   };
   return Cities;
 };
--- public/js/itinerary.js
+++ public/js/itinerary.js
@@ -32,11 +32,11 @@ $(eventSrchBtn).on("click", event => {
   let startDate = $(citySearchStartDate)
     .val()
     .trim();
-  let newStartDate = moment(startDate).format("YYYYDDMM");
+  let newStartDate = moment(startDate).format("YYYYMMDD");
   let endDate = $(citySearchEndDate)
     .val()
     .trim();
-  let newEndDate = moment(endDate).format("YYYYDDMM");
+  let newEndDate = moment(endDate).format("YYYYMMDD");
   let city = $(citySearch).val();
 
   eventAJAX(newStartDate, newEndDate, city);
@@ -68,15 +68,21 @@ function eventAJAX(newStartDate, newEndDate, city) {
       let address = response.events.event[i].venue_address;
       let start = response.events.event[i].start_time;
       let formatStart = moment(start).format("LLL");
+      let end = response.events.event[i].stop_time;
+      let formatEnd = moment(end).format("LLL");
       let url = response.events.event[i].url;
+      let cityname = response.events.event[i].city_name;
+      let region = response.events.event[i].region_name;
 
       var newCard = $(`<div class="card">`)
         .html(`      <div class="card-content" style="background-color: thistle;">
       <div class="content"><h1>${title}</h1>
         <p>
-          ${formatStart}
+         Event begins: ${formatStart}
         </p>
+        <p>Event ends: ${formatEnd}</p>
         <p>${address}</p>
+        <p>${cityname}, ${region}</p>
         <a href="${url}">Click Here to See More</a>
         <br />
       </div>
