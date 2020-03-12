4ee9e2c1792f6d86dc658d20c6302575b62a2f4d
Alyssa Cucunato <acucunato18@gmail.com>
2020-03-12 08:37:48 -0400

    added functionality for cards to append to members page from database

--- public/js/itinerary.js
+++ public/js/itinerary.js
@@ -49,84 +49,87 @@ handleNewItin = () => {
 
   $.post("/api/itineraries", newItin).then(function(data) {
     console.log(data);
+    // **** after user clicks SAVE routes to members page, where they can see their newly created itinerary
+    window.location.href = "/members";
   });
+};
 
-  // was: $.post('/api/newItin', newItin)
+// was: $.post('/api/newItin', newItin)
 
-  // if (url.indexOf("?post_id=") !== -1) {
-  //   postId = url.split("=")[1];
-  //   getPostData(postId, "post");
-  //   console.log(postId);
-  // }
+// if (url.indexOf("?post_id=") !== -1) {
+//   postId = url.split("=")[1];
+//   getPostData(postId, "post");
+//   console.log(postId);
+// }
 
-  //postItin();
-  //   if (updating) {
-  //     newItin.id = ItinId;
-  //     updateItin(newItin);
-  //   }
-  //   else {
-  //     postItin(newItin);
-  //   }
-  // }
+//postItin();
+//   if (updating) {
+//     newItin.id = ItinId;
+//     updateItin(newItin);
+//   }
+//   else {
+//     postItin(newItin);
+//   }
+// }
 
-  // postItin = () => {
-  //   app.post("/api/posts", () => {
-  //     db.Itineraries.create(req.body),then(dbItin => {
-  //       res.json(dbItin);
-  //       window.location.href = "/members";
-  //       console.log(dbItin);
-  //     });
-  //   });
-  // }
+// postItin = () => {
+//   app.post("/api/posts", () => {
+//     db.Itineraries.create(req.body),then(dbItin => {
+//       res.json(dbItin);
+//       window.location.href = "/members";
+//       console.log(dbItin);
+//     });
+//   });
+// }
 
-  $(eventSrchBtn).on("click", event => {
-    $("#event-response").empty();
-    let startDate = $(citySearchStartDate)
-      .val()
-      .trim();
-    let newStartDate = moment(startDate).format("YYYYMMDD");
-    let endDate = $(citySearchEndDate)
-      .val()
-      .trim();
-    let newEndDate = moment(endDate).format("YYYYMMDD");
-    let city = $(citySearch).val();
+$(eventSrchBtn).on("click", event => {
+  $("#event-response").empty();
+  let startDate = $(citySearchStartDate)
+    .val()
+    .trim();
+  let newStartDate = moment(startDate).format("YYYYMMDD");
+  let endDate = $(citySearchEndDate)
+    .val()
+    .trim();
+  let newEndDate = moment(endDate).format("YYYYMMDD");
+  let city = $(citySearch).val();
 
-    eventAJAX(newStartDate, newEndDate, city);
-  });
+  eventAJAX(newStartDate, newEndDate, city);
+});
 
-  function eventAJAX(newStartDate, newEndDate, city) {
-    event.preventDefault();
-    $("#event-response").empty();
+function eventAJAX(newStartDate, newEndDate, city) {
+  event.preventDefault();
+  $("#event-response").empty();
 
-    const api_key = "ZjB3tDMBWkFrcDq4";
-    const urlquery = "https://api.eventful.com/json/events/search?";
-    const searchurl = `${urlquery}app_key=${api_key}&location=${city}&within=10&date=${newStartDate}00-${newEndDate}00`;
+  const api_key = "ZjB3tDMBWkFrcDq4";
+  const urlquery = "https://api.eventful.com/json/events/search?";
+  const searchurl = `${urlquery}app_key=${api_key}&location=${city}&within=10&date=${newStartDate}00-${newEndDate}00`;
 
-    console.log(searchurl);
-    $.ajax({
-      url: searchurl,
-      dataType: "jsonp",
-      method: "POST"
-    })
-      .done(function(response) {
-        console.log(response);
-        // console.log(response.events.event);
-        if (response.status === 400 || response.events.length === 0) {
-          return $(".error-message").text("Oh No! No Events Found, Try Again.");
-        }
-        for (i = 0; i < response.events.event.length; i++) {
-          let title = response.events.event[i].title;
-          let address = response.events.event[i].venue_address;
-          let start = response.events.event[i].start_time;
-          let formatStart = moment(start).format("LLL");
-          let end = response.events.event[i].stop_time;
-          let formatEnd = moment(end).format("LLL");
-          let url = response.events.event[i].url;
-          let cityname = response.events.event[i].city_name;
-          let region = response.events.event[i].region_name;
+  console.log(searchurl);
+  $.ajax({
+    url: searchurl,
+    dataType: "jsonp",
+    method: "POST"
+  })
+    .done(function(response) {
+      console.log(response);
+      // console.log(response.events.event);
+      if (response.status === 400 || response.events.length === 0) {
+        return $(".error-message").text("Oh No! No Events Found, Try Again.");
+      }
+      for (i = 0; i < response.events.event.length; i++) {
+        let title = response.events.event[i].title;
+        let address = response.events.event[i].venue_address;
+        let start = response.events.event[i].start_time;
+        let formatStart = moment(start).format("LLL");
+        let end = response.events.event[i].stop_time;
+        let formatEnd = moment(end).format("LLL");
+        let url = response.events.event[i].url;
+        let cityname = response.events.event[i].city_name;
+        let region = response.events.event[i].region_name;
 
-          var newCard = $(`<div class="card">`)
-            .html(`      <div class="card-content" style="background-color: thistle;">
+        var newCard = $(`<div class="card">`)
+          .html(`      <div class="card-content" style="background-color: thistle;">
       <div class="content"><h1>${title}</h1>
         <p>
          Event begins: ${formatStart}
@@ -140,11 +143,10 @@ handleNewItin = () => {
     </div>
     </div>
     <br />`);
-          $("#event-response").append(newCard);
-        }
-      })
-      .catch(function() {
-        return $(".error-message").text("Oh No! No Events Found, Try Again.");
-      });
-  }
-};
+        $("#event-response").append(newCard);
+      }
+    })
+    .catch(function() {
+      return $(".error-message").text("Oh No! No Events Found, Try Again.");
+    });
+}
--- public/js/members.js
+++ public/js/members.js
@@ -10,6 +10,76 @@ $(document).ready(function() {
   // }
 });
 
-const postAReview = city => {
-  console.log(city);
-};
+let itinCardContainer = $(".itinerary-cards");
+
+// get new itin posts all itineraries from our database to the members page
+getNewItin();
+
+// New itinerary get route to append divs to page
+function getNewItin() {
+  $.get("api/itineraries", function(data) {
+    console.log(data);
+  })
+    .then(function(response) {
+      // reverse for loop adds newest itinerary at top
+      for (var i = response.length - 1; i >= 0; i--) {
+        let start = moment(response[i].start_date).format("l");
+        let end = moment(response[i].end_date).format("l");
+        let cityFromForm = response[i].city;
+        let food = response[i].food;
+        let activities = response[i].activities;
+        let nighttime = response[i].nighttime;
+        let reviews = response[i].reviews;
+        let id = response[i].id;
+
+        var itinCards = $(`          <div class="tile is-parent">`).html(
+          `<article class="tile is-child box">
+        <p class="title">
+          <i class="fas fa-map-marker-alt"></i> ${cityFromForm}
+        </p>
+        <p class="subtitle">${start}-${end}</p>
+        <p class="card-subhead">Food:</p>
+        ${food}
+        <p class="card-subhead">Activites:</p>
+        ${activities}
+        <p class="card-subhead">Nightlife:</p>
+        ${nighttime}
+        <p class="card-subhead">Reviews:</p>
+        <p>
+        ${reviews}
+        </p>
+        <div id="addReview${id}">
+          <!-- *****NEW REVIEW POSTED HERE**** -->
+        </div>
+        <br />
+        <div class="field">
+          <label class="label"
+            >Have you used this itinerary? If so leave a review.</label
+          >
+          <div class="control">
+            <!--Itin Review Textarea-->
+            <form id="itinRevInput${id}">
+              <textarea
+                id="${id}"
+                class="textarea"
+                placeholder="What did you enjoy or not enjoy this trip? Any suggestions for other travelers?"
+              ></textarea>
+              <button id="postReview${id}" class="button is-info is-light">
+                Post My Review
+              </button>
+            </form>
+          </div>
+        </div>
+      </article>
+    </div>`
+        );
+        $(".itinerary-cards").append(itinCards);
+      }
+      console.log(itinCards);
+    })
+    .catch(function() {
+      return $(".error-message").text(
+        "Oh No! There are no pre-existing Itineraries, Click Create Your Own to create one!"
+      );
+    });
+}
